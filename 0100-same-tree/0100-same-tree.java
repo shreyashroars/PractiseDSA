dad67550015f4d3a0d1e5ae8c887fac7a78d389b
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-11-19 15:20:48 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (86.89%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,34 +14,13 @@
  * }
  */
 class Solution {
-    String st="";
-    public void traverse(TreeNode root)
-    {
-        Queue<TreeNode>q=new LinkedList<>();
-        q.add(root);
-         while(!q.isEmpty())
-        {
-            TreeNode curr=q.poll();
-            st+=curr.val;
-            if(curr.left!=null)
-                q.add(curr.left);     
-            if(curr.right!=null)
-                q.add(curr.right);
-             if(!(curr.left==null &&curr.right==null) && curr.left==null)
-                 st+=" ";
-        }
-    }
+  
     public boolean isSameTree(TreeNode p, TreeNode q) {
-   
         if(p==null && q==null)
             return true;
         if(q==null ||p==null)
             return false;
-        traverse(p);
-        String temp=st;
-        st="";
-        traverse(q);
-        return st.equals(temp);       
+       return (p.val==q.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right));    
         
     }
 }

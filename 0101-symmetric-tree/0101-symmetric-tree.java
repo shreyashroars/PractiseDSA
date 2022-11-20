7ab472a80423d86c9ba2fdec60bbca76483ee2b2
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-11-20 11:46:17 +0530

    Time: 1 ms (78.49%), Space: 42.1 MB (67.44%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,68 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    Deque<Integer>st=new LinkedList<>();
-    public void trav(TreeNode root)
-    {
-        Queue<TreeNode>q=new LinkedList<>();
-        if(root==null)
-            return;
-        q.add(root);
-        while(!q.isEmpty())
-        {
-            int cnt=q.size();
-            for(int i=0;i<cnt;i++)
-            {
-                TreeNode curr=q.poll();
-                if(curr.left!=null)
-                {
-                    q.add(curr.left);
-                    st.offerLast(curr.left.val);
-                }
-                 if(curr.left==null)
-                 {
-                     st.offerLast(-101);
-                 }
-                if(curr.right!=null)
-                {
-                    q.add(curr.right);
-                        st.offerLast(curr.right.val);
-                }
-                if(curr.right==null)
-                {
-                     st.offerLast(-101);
-                }
-                   
-          
-        }
-              while(!st.isEmpty())
-              {
-                  if(st.peekFirst()==st.peekLast())
-                  {
-                      st.pollFirst();
-                      st.pollLast();
-                  }
-                  else
-                      return ;
-              }
-               
-    }
-    }
+   public boolean isSym(TreeNode node1,TreeNode node2)
+   {
+       if(node1==null && node2==null)
+           return true;
+       if(node1==null || node2==null)
+           return false;
+       if(node1.val!=node2.val)
+           return false;
+       return isSym(node1.left,node2.right) && isSym(node2.left,node1.right);
+   }
     public boolean isSymmetric(TreeNode root) {
-        trav(root);
-      return st.isEmpty();  
+        return isSym(root.left,root.right);
     }
 }

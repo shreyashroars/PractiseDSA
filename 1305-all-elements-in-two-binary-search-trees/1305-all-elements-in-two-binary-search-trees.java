b4b1d0ae3ff26afbc65cc1fc716f15451496688b
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-11-22 12:56:28 +0530

    Time: 22 ms (86.26%), Space: 67.9 MB (80.96%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,28 +14,60 @@
  * }
  */
 class Solution {
-    TreeMap<Integer,Integer>t=new TreeMap<>();
-    List<Integer>arr=new ArrayList<>();
-    
+   
+    List<Integer>arr1=new ArrayList<>();
+    List<Integer>arr2=new ArrayList<>();
+   List<Integer>arr=new ArrayList<>();
     public void inorder(TreeNode root)
     {
        if(root==null)
            return;
         inorder(root.left);
-        t.put(root.val,t.getOrDefault(root.val,0)+1);
+        arr1.add(root.val);
         inorder(root.right);
     }
+     public void inorder1(TreeNode root)
+    {
+       if(root==null)
+           return;
+        inorder1(root.left);
+      arr2.add(root.val);
+        inorder1(root.right);
+    }
+    
+    
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
      inorder(root1);
-     inorder(root2);
-    for(Map.Entry<Integer,Integer> entry : t.entrySet())
-    {
-         while(entry.getValue()>0)
-         {
-             arr.add(entry.getKey());
-             t.put(entry.getKey(),t.getOrDefault(entry.getKey(),0)-1);
-         }
-    }
+     inorder1(root2);
+        
+        int i=0,j=0;
+        
+        int n=arr1.size(),m=arr2.size();
+
+        while(i<n &&j<m)
+        {
+            if(arr1.get(i)<arr2.get(j))
+            {
+                arr.add(arr1.get(i));
+                i++;
+            }
+            else 
+            {
+                arr.add(arr2.get(j));
+                j++;
+            }
+          
+        }
+          while(i<n)
+            {
+               arr.add(arr1.get(i));
+                i++;
+            }
+            while(j<m)
+            {
+                arr.add(arr2.get(j));
+                j++;
+            }
         return arr;
     }
 }

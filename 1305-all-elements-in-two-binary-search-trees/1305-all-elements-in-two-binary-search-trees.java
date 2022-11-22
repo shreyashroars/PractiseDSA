441ef87d2e09fbae79b7caa14d67c1e597c0524f
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-11-22 12:57:45 +0530

    Time: 26 ms (80.23%), Space: 72.2 MB (48.18%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,60 +14,22 @@
  * }
  */
 class Solution {
-   
-    List<Integer>arr1=new ArrayList<>();
-    List<Integer>arr2=new ArrayList<>();
    List<Integer>arr=new ArrayList<>();
     public void inorder(TreeNode root)
     {
        if(root==null)
            return;
         inorder(root.left);
-        arr1.add(root.val);
+        arr.add(root.val);
         inorder(root.right);
     }
-     public void inorder1(TreeNode root)
-    {
-       if(root==null)
-           return;
-        inorder1(root.left);
-      arr2.add(root.val);
-        inorder1(root.right);
-    }
+    
     
     
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
      inorder(root1);
-     inorder1(root2);
-        
-        int i=0,j=0;
-        
-        int n=arr1.size(),m=arr2.size();
-
-        while(i<n &&j<m)
-        {
-            if(arr1.get(i)<arr2.get(j))
-            {
-                arr.add(arr1.get(i));
-                i++;
-            }
-            else 
-            {
-                arr.add(arr2.get(j));
-                j++;
-            }
-          
-        }
-          while(i<n)
-            {
-               arr.add(arr1.get(i));
-                i++;
-            }
-            while(j<m)
-            {
-                arr.add(arr2.get(j));
-                j++;
-            }
+     inorder(root2);
+    Collections.sort(arr);
         return arr;
     }
 }

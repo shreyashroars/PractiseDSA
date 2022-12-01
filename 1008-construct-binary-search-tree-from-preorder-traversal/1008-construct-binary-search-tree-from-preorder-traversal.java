a099c305888b6452562da862c84b8849d3e36501
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-12-01 23:31:27 +0530

    Time: 2 ms (16.01%), Space: 43.2 MB (6.47%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,16 +14,33 @@
  * }
  */
 class Solution {
-     int i = 0;
+    int preIndex=0;
+     TreeNode bstfromInorder(int[]pre,int[] in,int is,int ie)
+     {
+         if(is>ie)
+             return null;
+        
+         TreeNode node=new TreeNode(pre[preIndex++]);
+         int inIndex=0;
+         for(int i=is;i<=ie;i++)
+         {
+             if(in[i]==node.val)
+             {
+                 inIndex=i;
+                 break;
+             }
+         }
+         node.left=bstfromInorder(pre,in,is,inIndex-1);
+         node.right=bstfromInorder(pre,in,inIndex+1,ie);
+         return node;
+     }
     public TreeNode bstFromPreorder(int[] arr) {
-        return bstFromPreorder(arr, Integer.MAX_VALUE);
+        int in[]=new int[arr.length];
+        for(int i=0;i<arr.length;i++)
+            in[i]=arr[i];
+        Arrays.sort(in);
+        return bstfromInorder(arr,in,0,arr.length-1);
     }
 
-    public TreeNode bstFromPreorder(int[] arr, int bound) {
-        if (i == arr.length || arr[i] > bound) return null;
-        TreeNode root = new TreeNode(arr[i++]);
-        root.left = bstFromPreorder(arr, root.val);
-        root.right = bstFromPreorder(arr, bound);
-        return root;
-    }
+   
 }

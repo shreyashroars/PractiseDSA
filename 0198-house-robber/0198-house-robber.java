96a248cb98c12a6853fe570fa9eb0f7f1a5b064c
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-12-13 18:16:56 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (20.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,22 @@
 class Solution {
-     void dakaiti(int n,int[]nums,int[]dp)
-    {
-        dp[0]=nums[0];
-       for(int i=1;i<=n;i++)
+    
+    public int rob(int[] nums) {
+        int prev2=0,prev=nums[0],curr;
+       for(int i=1;i<nums.length;i++)
        {
            if(i==1)
            {
-               dp[1]=Math.max(nums[0],nums[1]);
+               curr=Math.max(nums[0],nums[1]);
+               prev2=nums[0];
+               prev=curr;
                continue;
            }
-       int pick=nums[i]+dp[i-2];
-       int ntpick=0+dp[i-1];
-         dp[i]=(int)Math.max(pick,ntpick);
-       }
+       int pick=nums[i]+prev2;
+       int ntpick=0+prev;
+         curr=(int)Math.max(pick,ntpick);
+           prev2=prev;
+           prev=curr;
     }
-    public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-     dakaiti(nums.length-1,nums,dp);
-        return dp[nums.length-1];
-    }
+        return prev;
+}
 }

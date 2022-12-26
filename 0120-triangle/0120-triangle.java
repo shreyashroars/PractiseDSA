844f5c9b6b66604eae20f452f2974d02a4620678
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-12-26 18:38:31 +0530

    Time: 4 ms (73.15%), Space: 41.6 MB (99.36%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,22 @@
 class Solution {
     int n;
-    public int rec_min(int i,int j,List<List<Integer>> t,int[][]dp)
-    {
-        if(i==n-1)
-            return t.get(i).get(j);
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-      int  d=t.get(i).get(j)+rec_min(i+1,j,t,dp);
-       int  dg=t.get(i).get(j)+rec_min(i+1,j+1,t,dp);
-        return dp[i][j]=d>dg?dg:d;
-    }
-    public int minimumTotal(List<List<Integer>> triangle) {
+    public int minimumTotal(List<List<Integer>> t) {
         
-        n=triangle.get(triangle.size()-1).size();
+        n=t.get(t.size()-1).size();
         int dp[][]=new int[n][n];
-        for(int[]rows:dp)
-            Arrays.fill(rows,-1);
-       return  rec_min(0,0,triangle,dp);
+        for(int j=0;j<=n-1;j++)
+            dp[n-1][j]=t.get(n-1).get(j);
+        for(int i=n-2;i>=0;i--)
+        {
+            int d=Integer.MAX_VALUE,dg=Integer.MAX_VALUE;
+            for(int j=i;j>=0;j--)
+            {
+          d=t.get(i).get(j)+dp[i+1][j];
+         dg=t.get(i).get(j)+dp[i+1][j+1];
+         dp[i][j]=d>dg?dg:d;
+                
+            }
+        }
+        return dp[0][0];
     }
 }

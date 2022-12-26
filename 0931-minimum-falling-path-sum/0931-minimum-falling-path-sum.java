aed70853bf1db482927e038e39d501e74ffcf9d9
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-12-26 22:42:37 +0530

    Time: 4 ms (73.79%), Space: 43.1 MB (70.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,26 +3,28 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         
          n=matrix.length;
-        int dp[][]=new int[n][n];
+        int prev[]=new int[n];
         for(int j=0;j<n;j++)
-       dp[0][j]=matrix[0][j];
+       prev[j]=matrix[0][j];
         for(int i=1;i<n;i++)
         {
+            int curr[]=new int[n];
             int up=1000000,left=1000000,right=1000000;
             for(int j=0;j<n;j++)
             {
-                up=matrix[i][j]+dp[i-1][j];
+                up=matrix[i][j]+prev[j];
                 if(j>0)
-                left=matrix[i][j]+dp[i-1][j-1];
+                left=matrix[i][j]+prev[j-1];
                 if(j<n-1)
-                right=matrix[i][j]+dp[i-1][j+1];
-                dp[i][j]=Math.min(up,Math.min(left,right));  
+                right=matrix[i][j]+prev[j+1];
+                curr[j]=Math.min(up,Math.min(left,right));  
             }
+            prev=curr;
         }
         int mini=Integer.MAX_VALUE;
         for(int i=0;i<n;i++)//This is the no. of column since ans can be from matrix[n-1][0] or //matrix[n-1][1]... and so on
         {
-            mini=Math.min(mini,dp[n-1][i]);
+            mini=Math.min(mini,prev[i]);
         }
         return mini;
     }

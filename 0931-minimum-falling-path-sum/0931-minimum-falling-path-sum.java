8e30345e6ea2f584039ee8277f8778ddb46110e4
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2022-12-26 22:38:17 +0530

    Time: 5 ms (59.13%), Space: 43.1 MB (70.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,28 @@
 class Solution {
     int n;
-    public int minFsum(int i,int j,int[][]mat,int[][]dp)
-    {
-        if(j<0||j>=n)
-            return 1000000;//returnning a maximum value
-        if(i==0)
-            return mat[i][j];
-        if(dp[i][j]!=-1000000)
-            return dp[i][j];
-        int up=mat[i][j]+minFsum(i-1,j,mat,dp);
-        int left=mat[i][j]+minFsum(i-1,j-1,mat,dp);
-         int right=mat[i][j]+minFsum(i-1,j+1,mat,dp);
-        return dp[i][j]=Math.min(up,Math.min(left,right));
-        
-    }
     public int minFallingPathSum(int[][] matrix) {
+        
          n=matrix.length;
         int dp[][]=new int[n][n];
-        for(int[]rows:dp)
-            Arrays.fill(rows,-1000000);
+        for(int j=0;j<n;j++)
+       dp[0][j]=matrix[0][j];
+        for(int i=1;i<n;i++)
+        {
+            int up=1000000,left=1000000,right=1000000;
+            for(int j=0;j<n;j++)
+            {
+                up=matrix[i][j]+dp[i-1][j];
+                if(j>0)
+                left=matrix[i][j]+dp[i-1][j-1];
+                if(j<n-1)
+                right=matrix[i][j]+dp[i-1][j+1];
+                dp[i][j]=Math.min(up,Math.min(left,right));  
+            }
+        }
         int mini=Integer.MAX_VALUE;
         for(int i=0;i<n;i++)//This is the no. of column since ans can be from matrix[n-1][0] or //matrix[n-1][1]... and so on
         {
-            mini=Math.min(mini,minFsum(n-1,i,matrix,dp));
+            mini=Math.min(mini,dp[n-1][i]);
         }
         return mini;
     }

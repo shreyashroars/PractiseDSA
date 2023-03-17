4dcd80a9c905b8ba6f0b88083e5d9698a8346b2c
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-03-17 10:05:41 +0530

    Time: 35 ms (94.22%), Space: 53.9 MB (32.77%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,28 +1,56 @@
-class Trie {
-   ArrayList<String>arr;
-    public Trie() {
-        arr=new ArrayList<>();
-    }
+class TrieNode {
+    boolean isCompleteWord;
+    TrieNode[] children;
     
-    public void insert(String word) {
-        arr.add(word);
-    }
-    
-    public boolean search(String word) {
-        return arr.contains(word);
-    }
-    
-    public boolean startsWith(String prefix) {
-        for(int i=0;i<arr.size();i++)
-        {
-            if(prefix.length()<=arr.get(i).length())
-            if(arr.get(i).substring(0,prefix.length()).equals(prefix))
-                return true;
-        }
-        return false;
+    public TrieNode() {
+        isCompleteWord = false; // when the word is complete (mark that node as true)
+        children = new TrieNode[26]; // for 26 possible Childrens (for next letter)
     }
 }
 
+class Trie {
+    TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isCompleteWord = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isCompleteWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie obj = new Trie();

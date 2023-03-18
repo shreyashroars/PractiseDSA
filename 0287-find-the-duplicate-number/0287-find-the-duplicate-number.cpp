a7cf08bdcad1fbb931c15ede13c011d2863354a4
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-03-18 18:26:49 +0530

    Time: 96 ms (97.61%), Space: 61.3 MB (31.24%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    int findMaxbit(int x)
-    {
-        int no_ofbits=0;
-        while(x>0)
-        {
-            x/=2;
-            no_ofbits++;
-        }
-        return no_ofbits;
-    }
     int findDuplicate(vector<int>& nums) {
-        int duplNum=0;
-        int n=nums.size()-1;
-        int maxnum=0;
-        for(int i=0;i<n;i++)
-        {
-            maxnum=max(maxnum,nums[i]);
+
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        int maxbits=findMaxbit(maxnum);
-        for(int i=0;i<maxbits;i++)
-        {
-            int mask=1<<i;
-            int base_cnt=0,nums_cnt=0;
-            for(int j=0;j<=n;j++)
-            {
-                if((j&mask))
-                    base_cnt++;
-                if((nums[j]& mask))
-                    nums_cnt++;
-            }
-            if(nums_cnt>base_cnt)
-                duplNum|=mask;
-            
-        }
-        return duplNum;
+
+        return hare;
     }
 };

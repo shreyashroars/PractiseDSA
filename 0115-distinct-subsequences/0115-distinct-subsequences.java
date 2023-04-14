73d8a79936dceb4047839361cecbfe2de19f4a48
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-04-14 12:00:37 +0530

    Time: 15 ms (88.98%), Space: 40.6 MB (92.87%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,18 +3,14 @@ class Solution {
        int n1 = s1.length();
         int n2 = s2.length();
         int[] prev = new int[n2+1];
-        int[] curr = new int[n2+1];
-        prev[0] = curr[0] = 1;
+        prev[0] =1;
         
         for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+            for (int j = n2; j >=1; j--) {
                 if (s1.charAt(i-1) == s2.charAt(j-1)) {
-                    curr[j] = prev[j-1] + prev[j];
-                } else {
-                    curr[j] = prev[j];
-                }
+                    prev[j] = prev[j-1] + prev[j];
+                } 
             }
-            prev = curr.clone();
         }
         
         return prev[n2];

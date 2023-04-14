3404a83404e02d1befd1696fce246a5c3d1bb720
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-04-14 11:50:54 +0530

    Time: 20 ms (52.71%), Space: 43 MB (86.80%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,35 +1,27 @@
 class Solution {
-    String s1,s2;
-    public int numDistinct(String s, String t) {
-        s1=s;
-        s2=t;
-        int n1=s.length();
-        int n2=t.length();
-        int dp[][]=new int[n1+1][n2+1];
-        for(int i=0;i<=n1;i++)
-            dp[i][0]=1;
-        for(int i=1;i<=n2;i++)
-            dp[0][i]=0;
-        for(int i=1;i<=n1;i++)
-        {
-            for(int j=1;j<=n2;j++)
-            {
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
-        }
-    //     for(int i=0;i<=n1;i++)
-    //     {
-    //         for(int j=0;j<=n2;j++)
-    //         {
-    //             System.out.print(dp[i][j]+" ");
-    //         }
-    //     System.out.println();
-    // }
+    public int numDistinct(String s1, String s2) {
+       int n1 = s1.length();
+        int n2 = s2.length();
+        int[] prev = new int[n2+1];
+        int[] curr = new int[n2+1];
+        prev[0] = curr[0] = 1;
         
-       return  dp[n1][n2];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    curr[j] = prev[j-1] + prev[j];
+                } else {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr.clone();
+        }
+        
+        return prev[n2];
+
+
+
+
         
     }
 }

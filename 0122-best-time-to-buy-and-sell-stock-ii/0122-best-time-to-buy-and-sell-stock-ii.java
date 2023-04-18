9788f6ff5826d4309524f7e4374ac73627ba6709
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-04-18 12:54:25 +0530

    Time: 1 ms (95.92%), Space: 43 MB (16.05%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,33 @@
 class Solution {
    int profit=-2;
     public int maxProfit(int[] prices) {
-        int dp[][]=new int[prices.length+1][2];
-        dp[prices.length][0]=dp[prices.length][1]=0;
+       
+        int ahead[]=new int[2];
+        int curr[]=new int[2];
+        ahead[0]=ahead[1]=0;
        for(int ind=prices.length-1;ind>=0;ind--)
        {
            for(int buy=0;buy<=1;buy++)
            {
             if(buy==1)
        {
-           profit=Math.max(-prices[ind]+dp[ind+1][0]//bought the stock on that day
-                           ,0+dp[ind+1][1]//not bought the stock on that day
+           profit=Math.max(-prices[ind]+ahead[0]//bought the stock on that day
+                           ,0+ahead[1]//not bought the stock on that day
                           );
     }
     else
     {
-        profit=Math.max(prices[ind]+dp[ind+1][1]//bought the stock on that day
-                           ,0+dp[ind+1][0]//not bought the stock on that day
+        profit=Math.max(prices[ind]+ahead[1]//bought the stock on that day
+                           ,0+ahead[0]//not bought the stock on that day
                           );
         
     }
-        dp[ind][buy]=profit;
+       curr[buy]=profit;
        }
+          ahead=curr;
        }
            
         
-      return dp[0][1];
+      return ahead[1];
     }
 }

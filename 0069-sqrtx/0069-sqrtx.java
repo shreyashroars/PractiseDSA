2965f7a64d16bf678aa8ccf17a6ce8c2cb5a823f
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-05-10 00:06:23 +0530

    Time: 20 ms (14.46%), Space: 42.1 MB (7.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,34 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+          long ans=0;
+        if(x==1)
+            return 1;
+      
+        else
+        {
+            int y=x/2;
+            long low=1,high=y;
+            while(low<=high)
+            {
+                long mid=(high-low)/2+low;
+                if(mid*mid==x)
+                {
+                    System.out.println(mid);
+                    return (int)mid;
+                }
+                else if(mid*mid<x)
+                {
+                    ans=mid;
+                    System.out.println(ans);
+                    low=mid+1;
+                }
+                else
+                    high=mid-1;
+                    
+            }
+         
+            return (int)ans;
+        }
         
     }
 }

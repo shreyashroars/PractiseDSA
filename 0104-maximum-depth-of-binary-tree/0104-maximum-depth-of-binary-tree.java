087cd3163e0090ab9582ed1a76fe008ee1df14f8
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-07-31 15:39:36 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (76.88%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,9 +14,13 @@
  * }
  */
 class Solution {
+    public int height(TreeNode root)
+    {
+        if(root==null)
+            return 0;
+        return Math.max(height(root.left),height(root.right))+1;
+    }
     public int maxDepth(TreeNode root) {
-       if(root==null)
-           return 0;
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+       return height(root); 
     }
 }

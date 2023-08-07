c46c20aa78ce48809e6f8d22c554afb95a5693fe
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-07 10:42:19 +0530

    Time: 1 ms (49.25%), Space: 54.9 MB (95.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,58 +1,17 @@
-class ListNode
-{
-    int val;
-    ListNode next;
-    ListNode(int val)
-    {
-        this.val=val;
-    }
-}
+
 class Solution {
-    public void rotate(int[] nums, int k) {
-        ListNode head=null,temp=head;
-        for(int x:nums)
+    public void rotate(int[] arr, int k) {
+        int n=arr.length;
+     int arr1[] =new int[n] ;
+        int i1=0;
+        for(int i=0;i<n;i++)
         {
-            if(head==null)
-            {
-                head=new ListNode(x);
-                temp=head;
-            }
-            else
-            {
-                temp.next=new ListNode(x);
-                temp=temp.next;
-            }
+            i1=(i+k)%n;
+            arr1[i1]=arr[i];
         }
-         temp=head;
-        int n=0;
-        while(temp!=null)
+        for(int i=0;i<n;i++)
         {
-            temp=temp.next;
-            n++;
+            arr[i]=arr1[i];
         }
-        k=k%n;
-        n=n-k;
-        temp=head;
-        while(n>1)
-        {
-            temp=temp.next;
-            n--;
-        }
-        ListNode last=temp;
-        while(last.next!=null)
-        {
-            last=last.next;
-        }
-        last.next=head;
-        head=temp.next;
-        temp.next=null;
-        int i=0;
-       while(head!=null)
-       {
-           nums[i++]=head.val;
-           head=head.next;
-       }
-        
-    }
-    
+}
 }

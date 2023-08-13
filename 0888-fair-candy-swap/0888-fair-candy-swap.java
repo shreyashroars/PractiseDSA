9b8d0a67180e25ac106f927284e69645573f90c5
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-13 19:27:26 +0530

    Time: 17 ms (47.13%), Space: 45.3 MB (62.00%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,4 +1,22 @@
 class Solution {
+    public int binSearch(int arr[],int key)
+    {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=low-(low-high)/2;
+            if(arr[mid]==key)
+                return mid;
+            else if(arr[mid]>key)
+            {
+                high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return -1;
+    }
     public int[] fairCandySwap(int[] arr1, int[] arr2) {
         int pSum1=arr1[0];
         int pSum2=arr2[0];
@@ -8,18 +26,13 @@ class Solution {
             pSum1+=arr1[i];
         for(int i=1;i<n2;i++)
             pSum2+=arr2[i];
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         for(int i=0;i<n1;i++)
         {
-            pSum1=pSum1-arr1[i];
-            for(int j=0;j<n2;j++)
-            {
-                 pSum2=pSum2-arr2[j];
-                if(pSum1+arr2[j]==pSum2+arr1[i])
-                    return new int[]{arr1[i],arr2[j]};
-                 pSum2+=arr2[j];
-            }
-            pSum1+=arr1[i];
-           
+            int req=(pSum2-pSum1)/2+arr1[i];
+            if(binSearch(arr2,req)!=-1)
+                return new int[]{arr1[i],req};  
         }
         return new int[2];
         

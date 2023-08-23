f6a2ea1e6120c432afe394bc8e06201e44ffe87a
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-23 23:01:58 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (82.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int rec(int i,int j,int dp[][])
-    {
-        if(i==0 && j==0)
-            return 1;
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        return dp[i][j]=rec(i-1,j,dp)+rec(i,j-1,dp);
-    }
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        dp[0][0]=1;
+       int prev[]=new int[n];
         for(int i=0;i<m;i++)
         {
+            int curr[]=new int[n];
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
-                    continue;
+                    curr[0]=1;
+                else
+                {
                 int a=0;
                 if(i>=1)
-                    a=dp[i-1][j];
+                    a=prev[j];
                 int b=0;
                 if(j>=1)
-                    b=dp[i][j-1];
-                dp[i][j]=a+b;
+                    b=curr[j-1];
+                curr[j]=a+b;
+                }
+               
             }
+             prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }

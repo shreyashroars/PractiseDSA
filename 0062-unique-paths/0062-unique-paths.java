64c4716b70c9a099ebbca94889df38900bdb916d
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-23 21:33:24 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (38.40%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,18 @@
 class Solution {
+    public int rec(int i,int j,int dp[][])
+    {
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=rec(i-1,j,dp)+rec(i,j-1,dp);
+    }
     public int uniquePaths(int m, int n) {
-        int dp[]=new int[n];
-        for(int i=0;i<m;i++)
-        {
-            int temp[]=new int[n];
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 && j==0)
-                {
-                    temp[0]=1;
-                    continue;
-                }
-                int up=0,left=0;
-                if(i-1>=0)
-                 up=dp[j];
-                if(j-1>=0)
-                 left=temp[j-1];
-                temp[j]=up+left;
-            }
-            dp=temp;
-        }
-        return dp[n-1];
+        int dp[][]=new int[m+1][n+1];
+        for(int rows[]:dp)
+            Arrays.fill(rows,-1);
+        return rec(m-1,n-1,dp);  
     }
 }

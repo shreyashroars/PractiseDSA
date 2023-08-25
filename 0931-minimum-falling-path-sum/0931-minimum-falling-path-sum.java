e20ec967d94c10ae7d8c8966eeb3106efc16a7a9
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-26 00:23:32 +0530

    Time: 1 ms (98.23%), Space: 44.3 MB (48.58%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,22 @@
 class Solution {
-    int n;
-    public int minFallingPathSum(int[][] matrix) {
-        
-         n=matrix.length;
-        int prev[]=new int[n];
-        for(int j=0;j<n;j++)
-       prev[j]=matrix[0][j];
-        for(int i=1;i<n;i++)
-        {
-            int curr[]=new int[n];
-            int up=1000000,left=1000000,right=1000000;
-            for(int j=0;j<n;j++)
-            {
-                up=matrix[i][j]+prev[j];
-                if(j>0)
-                left=matrix[i][j]+prev[j-1];
-                if(j<n-1)
-                right=matrix[i][j]+prev[j+1];
-                curr[j]=Math.min(up,Math.min(left,right));  
-            }
-            prev=curr;
-        }
-        int mini=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++)//This is the no. of column since ans can be from matrix[n-1][0] or //matrix[n-1][1]... and so on
-        {
-            mini=Math.min(mini,prev[i]);
-        }
-        return mini;
+    public int rec(int[][] arr,int i,int j,int n,int dp[][])
+    {
+        if(i<0 ||j<0 ||i>=n||j>=n)
+            return Integer.MAX_VALUE;
+        if(i==n-1)
+            return arr[i][j];
+        if(dp[i][j]!=-1000000)
+            return dp[i][j];
+        return dp[i][j]=Math.min(rec(arr,i+1,j+1,n,dp),Math.min(rec(arr,i+1,j,n,dp),rec(arr,i+1,j-1,n,dp)))+arr[i][j];
+    }
+    public int minFallingPathSum(int[][] arr) {
+        int n=arr.length;
+        int dp[][]=new int[n][n];
+        int min=Integer.MAX_VALUE;
+        for(int rows[]:dp)
+            Arrays.fill(rows,-1000000);
+        for(int i=0;i<n;i++)
+        min=Math.min(min,rec(arr,0,i,n,dp));
+        return min;
     }
 }

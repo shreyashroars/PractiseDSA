df3aed6e6585f7a583932e528033fbb27b9d94f9
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-08-29 00:37:34 +0530

    Time: 5 ms (90.39%), Space: 43.8 MB (25.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -12,23 +12,39 @@ class Solution {
 // 		return dp[ind][prevInd+1]=Math.max(take,not_take);
 
 // 	}
-    public int lengthOfLIS(int[] arr) {
-        	int n=arr.length;
-		   int dp[][]=new int[n+1][n+1];
-        for(int ind=n-1;ind>=0;ind--)
+    public int findCeil(ArrayList<Integer>arr,int target)
+    {
+        int low=0;
+        int high=arr.size()-1;
+        int ans=0;
+        while(low<=high)
         {
-            for(int prevInd=ind-1;prevInd>=-1;prevInd--)
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)>=target)
             {
-                int not_take=dp[ind+1][prevInd+1];
-		        int take=-(int)1e9;
-		        if(prevInd==-1 ||arr[ind]>arr[prevInd])
-                    take=1+dp[ind+1][ind+1];
-		        dp[ind][prevInd+1]=Math.max(take,not_take);
-                
+                ans=mid;
+                high=mid-1;
+            }
+            else if(arr.get(mid)<target)
+                low=mid+1;
+        }
+        return ans;
+    }
+    public int lengthOfLIS(int[] arr) {
+        int n=arr.length;
+        ArrayList<Integer>temp=new ArrayList<>();
+        temp.add(arr[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>temp.get(temp.size()-1))
+                temp.add(arr[i]);
+            else
+            {
+                int ind=findCeil(temp,arr[i]);
+                temp.set(ind,arr[i]);
             }
         }
-        return dp[0][-1+1];
-        
+        return temp.size();
         
     }
 }

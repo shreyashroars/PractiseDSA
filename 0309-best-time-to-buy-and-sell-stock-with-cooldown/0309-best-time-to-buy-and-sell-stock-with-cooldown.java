6345f0e7c77d9eff783881c89021f472cfae9b34
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-09-02 22:42:52 +0530

    Time: 1 ms (86.68%), Space: 40.8 MB (36.97%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,34 +1,38 @@
 class Solution {
-    public int rec(int ind,int buy,int canBuy,int arr[],int n,int dp[][][])
-    {
-        if(ind==n)
-        return 0;
-        if(dp[ind][buy][canBuy]!=-1)
-        return dp[ind][buy][canBuy];
-        if(buy==1)
+    public int maxProfit(int[] arr) {
+        int n=arr.length;
+        int dp[][][]=new int[n+1][2][2];
+        dp[n][0][0]=0;
+        dp[n][0][1]=0;
+        dp[n][1][0]=0;
+        dp[n][1][1]=0;
+        for(int ind=n-1;ind>=0;ind--)
         {
-            int not_take=rec(ind+1,1,1,arr,n,dp);
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int canBuy=0;canBuy<=1;canBuy++)
+                {
+                    if(buy==1)
+        {
+            int not_take=dp[ind+1][1][1];
             int take=0;
             if(canBuy==1)
-            take=-arr[ind]+rec(ind+1,0,1,arr,n,dp);
-            return dp[ind][buy][canBuy]=Math.max(take,not_take);
+            take=-arr[ind]+dp[ind+1][0][1];
+            dp[ind][buy][canBuy]=Math.max(take,not_take);
         }
         else
         {
-            int not_take=rec(ind+1,0,1,arr,n,dp);
-            int take=arr[ind]+rec(ind+1,1,0,arr,n,dp);
-            return dp[ind][buy][canBuy]=Math.max(take,not_take);
+            int not_take=dp[ind+1][0][1];
+            int take=arr[ind]+dp[ind+1][1][0];
+            dp[ind][buy][canBuy]=Math.max(take,not_take);
         }
-    }
-    public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int dp[][][]=new int[n][2][2];
-        for(int arr[][]:dp)
-        {
-            for(int rows[]:arr)
-            Arrays.fill(rows,-1);
+                    
+                    
+                }
+            }
         }
-       return rec(0,1,1,prices,n,dp);
+        
+       return dp[0][1][1];
         
     }
 }

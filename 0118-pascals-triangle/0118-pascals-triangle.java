01053398161aa0aa0828d881e211e5cc67a6b004
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-09-09 19:32:58 +0530

    Time: 1 ms (89.15%), Space: 41.3 MB (25.66%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,11 +3,12 @@ class Solution {
         List<List<Integer>>arr=new ArrayList<>();
         for(int i=0;i<n;i++)
         {
-            
             List<Integer>arr1=Arrays.asList(new Integer[i+1]);
-            Collections.fill(arr1,1);
-            for(int j=1;j<i;j++)
+            for(int j=0;j<=i;j++)
             {
+                if(j==0||j==i)
+                    arr1.set(j,1);
+                else
                 arr1.set(j,arr.get(i-1).get(j)+arr.get(i-1).get(j-1));
             }
             arr.add(arr1);

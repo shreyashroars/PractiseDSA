1e7dbdc7b28f50165dc70ff513385cc1e8d350e1
Shreyash Pandey <81769088+shreyashroars@users.noreply.github.com>
2023-10-21 18:42:14 +0530

    Added solution - LeetHub

---
+++ Stack using two queues - GFG/stack-using-two-queues.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class StackUsingQueues
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			Queues g = new Queues();
+			
+			int q = sc.nextInt();
+			while(q>0)
+			{
+				int QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					g.push(a);
+				}
+				else if(QueryType == 2)
+				System.out.print(g.pop()+" ");
+			q--;
+			}	
+			System.out.println();
+				
+			
+			
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+
+class Queues
+{
+    Queue<Integer> q = new LinkedList<Integer>();
+    //Function to push an element into stack using two queues.
+    void push(int a)
+    {
+	    q.add(a);
+	    for(int i=1;i<=q.size()-1;i++)
+	    q.add(q.poll());
+    }
+    
+    //Function to pop an element from stack using two queues. 
+    int pop()
+    {
+	    return q.isEmpty()?-1:q.poll();
+    }
+	
+}
+
